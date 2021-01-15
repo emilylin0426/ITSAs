a12fa85ed16b834b73605e37573a85d26a09a442
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 13:01:56 +0800

    Update 36.c

--- 36.c
+++ 36.c
@@ -3,12 +3,12 @@
 int main(){
     int year;
     while(scanf("%d", &year)!=EOF){
-        if(year%4 == 0){
-            if(year%400 == 0)printf("Bissextile Year\n");
-            else if(year%100 == 0)printf("Common Year\n");
-            else printf("Bissextile Year\n");
+        if(year%4 == 0){                                        //如果是4的倍數
+            if(year%400 == 0)printf("Bissextile Year\n");       //逢400閏
+            else if(year%100 == 0)printf("Common Year\n");      //逢100不閏
+            else printf("Bissextile Year\n");                   //和100.400無關的話就閏年
         }
-        else printf("Common Year\n");
+        else printf("Common Year\n");                           //不是4的倍數就不是閏年
     }
     return 0;
 }
