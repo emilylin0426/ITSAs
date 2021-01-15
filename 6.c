ed04b7aa3c864e345fa362b1ae7f9398c23cb769
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 14:17:30 +0800

    Update 6.c

--- 6.c
+++ 6.c
@@ -3,10 +3,10 @@
 int main(){
     int month;
     while(scanf("%d", &month)!=EOF){
-        if(month >=3 && month <=5)printf("Spring\n");
-        else if(month >=6 && month <= 8)printf("Summer\n");
-        else if(month >=9 && month <= 11)printf("Autumn\n");
-        else if(month == 12 || month == 1 || month == 2)printf("Winter\n");
+        if(month >=3 && month <=5)printf("Spring\n");                           //如果是3~5月，就是春天
+        else if(month >=6 && month <= 8)printf("Summer\n");                     //6~8是夏天
+        else if(month >=9 && month <= 11)printf("Autumn\n");                    //以此類推
+        else if(month == 12 || month == 1 || month == 2)printf("Winter\n");     //每三個月一個季節啦
     }
     return 0;
 }
