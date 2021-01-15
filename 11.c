f2b15473dbbe7845ad154be2b0e09ee63365f699
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 11:40:43 +0800

    Update 11.c

--- 11.c
+++ 11.c
@@ -6,13 +6,13 @@ int main(){
         int matrix[row][col];
         for(i=0; i<row; i++){
             for(j=0; j<col; j++){
-                scanf("%d", &matrix[i][j]);
+                scanf("%d", &matrix[i][j]);     //讀入矩陣
             }
         }
-        for(i=0; i<col; i++){
-            for(j=0; j<row-1; j++){
-                printf("%d ", matrix[j][i]);
-            }
+        for(i=0; i<col; i++){                   //輸出時，row&col反轉(外圈迴圈為col、內圈row)
+            for(j=0; j<row-1; j++){             //迴圈的ij控制的是行列要印的個數
+                printf("%d ", matrix[j][i]);    //印出矩陣時也要反轉row&col，在印第i列第j行時，要印出原本矩陣的第j列第i行   
+            }                                   
             printf("%d\n", matrix[j][i]);
         }
     }
