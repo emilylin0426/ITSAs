b63ac0c3fea5802624a3c8e3df9c3a0ba3418323
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 11:55:50 +0800

    Update 16.c

--- 16.c
+++ 16.c
@@ -7,11 +7,11 @@ int main(){
     scanf("%s", line);
     char *ptr;
     int cnt=0;
-    ptr = line;
-    while(ptr = strstr(ptr, target)){
-        ptr++;
-        cnt++;
-    }
+    ptr = line;                                 //ptr指向line的起始位置
+    while(ptr = strstr(ptr, target)){           //如果在ptr字串中有找到target字串
+        ptr++;                                  //則ptr往後一位(看有沒有第二次重複)
+        cnt++;                                  //計數+1
+    }                                           //再重複一次找尋動作，直到strstr在ptr中找不到target字串而回傳NULL，跳出迴圈
     printf("%d\n", cnt);
     return 0;
 }
