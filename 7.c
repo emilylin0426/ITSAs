3f1cbeeb9356732f8f1e3c24254cc4fe42a018f7
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 14:20:31 +0800

    Update 7.c

--- 7.c
+++ 7.c
@@ -8,12 +8,12 @@ int main(){
     for(i=0; i<cnt; i++){
         scanf(" %c%d%d%d%d",&opr, &a1, &b1, &a2, &b2);
         if(opr == '+'){
-            printf("%d %d\n", a1+a2, b1+b2);
+            printf("%d %d\n", a1+a2, b1+b2);                    //複數的加法：實部加實部、虛部加虛部
         }
-        else if(opr == '-'){
+        else if(opr == '-'){                                    //減法：實部減實部、虛部減虛部
             printf("%d %d\n", a1-a2, b1-b2);
         }
-        else if(opr == '*'){
+        else if(opr == '*'){                                    //實部*實部=虛部*虛部=實數、虛部*實部=虛數
             printf("%d %d\n", a1*a2 - b1*b2, a1*b2 + b1*a2);
         }
     }
