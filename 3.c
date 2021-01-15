5e06e4358af8410cca99f7c1d619c80f45b4c7aa
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 12:37:17 +0800

    Update 3.c

--- 3.c
+++ 3.c
@@ -3,8 +3,8 @@
 int main(){
     int x,y;
     while(scanf("%d %d", &x, &y)!=EOF){
-        if( (x*x) + (y*y) > (100*100) )printf("outside\n");
-        else printf("inside\n");
+        if( (x*x) + (y*y) > (100*100) )printf("outside\n");     //如果到原點的距離>元的半徑的話，此點就在外面
+        else printf("inside\n");                                //反之點在裡面
     }
     return 0;
 }
