9d291c88347e9d5373cb661b9f7cdb364929cde6
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 11:43:45 +0800

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int func(int n){
-    if(n == 0 || n == 1)return n+1;
-    else if(n>1){
+int func(int n){                        //func遞迴函式
+    if(n == 0 || n == 1)return n+1;     //根據題目給的公式，當n=1或2時回傳n+1
+    else if(n>1){                       //當n>1，f(n)=f(n-1)+f(n/2)
         return func(n-1) + func(n/2);
     }
 }
@@ -10,7 +10,7 @@ int func(int n){
 int main(){
     int input;
     while(scanf("%d", &input)!=EOF){
-        printf("%d\n", func(input));
+        printf("%d\n", func(input));    //印出結果
     }
     return 0;
 }
