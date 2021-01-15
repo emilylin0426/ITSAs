884afce1a79a3098d8274de9412fdfc138f5f635
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 14:14:34 +0800

    Update 5.c

--- 5.c
+++ 5.c
@@ -3,18 +3,17 @@
 int main(){
     int input, output[8]={0}, i;
     while(scanf("%d", &input)!=EOF){
-        if(input<0){
-            output[0] = 1;
-            input += 256;
+        if(input<0){                        //如果是負數
+            output[0] = 1;                  //二進位的第一碼為1
+            input += 256;                   //然後把負的推移成正的
         }
-        //printf("enter\n");
-        for(i=7; i>0; i--){
-            output[i] = input % 2; 
-            input /= 2;
+        for(i=7; i>0; i--){                 //一位一位key in(由後往前)
+            output[i] = input % 2;          //把十進位轉成2進位
+            input /= 2;                     //每除一次2，二進位制就往前一格
         }
         
         for(i=0; i<7; i++){
-            printf("%d", output[i]);
+            printf("%d", output[i]);        //印出結果
         }
         printf("%d\n", output[i]);
     }
