24fe4d45e0b446106aadecf138b743502bd8fe77
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 11:57:14 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -3,7 +3,7 @@
 int main(){
     double input;
     while(scanf("%lf", &input)!=EOF){
-        printf("%.1lf\n", input*1.6);
+        printf("%.1lf\n", input*1.6);           //將英哩*1.6變成公里後印出到小數點後一位
     }
     return 0;
 }
