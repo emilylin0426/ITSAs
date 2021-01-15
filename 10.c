2bb73cbd636d914f07599c2ced573615fb8b29b1
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 11:33:29 +0800

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <stdio.h>	
 int main(){
 	int X,Y;
 	int a;
 	scanf("%d %d",&X,&Y);
 	if(X>Y)a=X;
 	else a=Y;
-	for(a==X||a==Y; X%a!=0 || Y%a!=0 ;a--);
+	for(a==X||a==Y; X%a!=0 || Y%a!=0 ;a--);		//若XY不能同時整除a，則往下減1繼續找，當跳出迴圈表示找到公因數a(XY都能整除a)
 	printf("%d\n",a);
 }
