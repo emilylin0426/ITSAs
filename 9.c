3e182861fffe1db97cc1d10c662e77a63ecb72cc
emilylin0426 <77456875+emilylin0426@users.noreply.github.com>
2021-01-15 14:24:10 +0800

    Update 9.c

--- 9.c
+++ 9.c
@@ -5,11 +5,11 @@ int main(){
     while(scanf("%d", &n)!=EOF){
         int ans=0;
         for(i=3; i<=n; i++){
-            if(i%3 == 0){
-                ans += i;
+            if(i%3 == 0){           //從3開始，如果遇到3的倍數
+                ans += i;           //就累加
             }
         }
-        printf("%d\n", ans);
+        printf("%d\n", ans);        //印出累加的結果
     }
     return 0;
 }
